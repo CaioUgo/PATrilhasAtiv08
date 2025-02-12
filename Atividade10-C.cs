5ba34dd96526aa07928a707452ff0102d096090b
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-02-12 08:28:56 -0300

    Atividade10-C.cs

---
+++ Atividade10-C.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+	static int Soma(int num1, int num2)
+	{
+		return num1 + num2;
+	}
+	
+	static void Main()
+	{
+		Console.WriteLine("Digite o primeiro número: ");
+		int	numero1 = int.Parse(Console.ReadLine());
+		
+		Console.WriteLine("\n\rDigite o segundo número: ");
+		int	numero2 = int.Parse(Console.ReadLine());
+		
+		Console.Clear();
+		
+		int soma = Soma(numero1, numero2);
+		
+		Console.WriteLine("\n\rA soma dos numeros {0} + {1} é: {2}", numero1, numero2, soma);
+		Console.WriteLine("\n\rPressione qualquer tecla para encerrar.....");
+		Console.ReadKey(true);			
+  }	
+    }
+				
